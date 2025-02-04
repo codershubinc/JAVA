a3b89ab9930137fb98f6c423d2d53ddcf806ec6e
swapnil ingle <ingleswapnil2004@gmail.com>
2025-02-04 23:28:26 +0530

    Add initial Java files for HelloWorld and Main, and create README

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+
+        // Example method calls
+        printName("Alice");
+        printSum(5, 10);
+    }
+
+    public static void printName(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void printSum(int a, int b) {
+        System.out.println("Sum: " + (a + b));
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Java Learning Project
+
+Welcome to the Java Learning Project!
+
+# Happy Learning!
